d75b75312ebec80f67e394e65b8756b51b9a85d9
guanluocang <guanluocang@163.com>
2017-10-20 09:18:27 +0800

    contro业务逻辑

---
+++ app/src/main/java/com/example/explosiverobot/control/IControPresenter.java
@@ -0,0 +1,11 @@
+package com.example.explosiverobot.control;
+
+import com.example.explosiverobot.base.BasePresenter;
+
+/**
+ * Created by dell on 2017/10/19.
+ */
+
+public abstract class IControPresenter implements BasePresenter {
+
+}
--- app/src/main/java/com/example/explosiverobot/control/IControlView.java
+++ app/src/main/java/com/example/explosiverobot/control/IControlView.java
@@ -1,10 +1,11 @@
-//package com.example.explosiverobot.control;
-//
-//import com.ocean.mvp.library.view.UiView;
-//
-///**
-// * Created by dell on 2017/10/16.
-// */
-//
-//public interface IControlView extends UiView {
-//}
+package com.example.explosiverobot.control;
+
+
+import com.example.explosiverobot.base.BaseView;
+
+/**
+ * Created by dell on 2017/10/16.
+ */
+
+public interface IControlView extends BaseView {
+}
